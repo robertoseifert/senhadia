d85797d4f33532a07ba00730a6ad968f0e771023
robertoseifert <73675276+robertoseifert@users.noreply.github.com>
2024-01-11 09:19:52 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from datetime import datetime
+
+def calcular_senha_do_dia():
+    hoje = datetime.now()
+    senha_do_dia = hoje.day * hoje.month * int(str(hoje.year)[-2:]) * 3
+    return senha_do_dia
+
+senha = calcular_senha_do_dia()
+
+# Define o tamanho do texto usando HTML
+st.markdown(f"<h1 style='color: #FFF;'>Senha do Dia: {senha}</h1>", unsafe_allow_html=True)
+st.markdown("[Builds Mobile](https://builds.unipluscdn.com/unimobile/)")
+st.markdown("[Builds Uniplus](https://utilitarios.intelidata.inf.br/)")
+st.markdown("[Wiki intelidata](https://centraldouniplus.intelidata.inf.br/)")
+st.markdown("[Não clique aqui](https://www.4oito.com.br/media/posts/ananda-figueiredo-voce-e-teimoso_66.jpg)")
+
+
